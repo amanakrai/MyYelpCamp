bf2e211f2fa9f2cf973933fe939a690d17833531
Aman <amanakrai98@gmail.com>
2020-07-27 08:24:26 +0000

    add MongoAtls

--- app.js
+++ app.js
@@ -19,10 +19,20 @@ var indexRoutes		 =require("./routes/index");
 //========================
 //Connection to MongoDB
 //========================
-mongoose.connect('mongodb://localhost:27017/yelp_camp', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+// mongoose.connect('mongodb://localhost:27017/yelp_camp', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// })
+mongoose.connect('mongodb+srv://amanakrai:aman123rai@cluster0.ll2jn.mongodb.net/MyYelpCamp?retryWrites=true&w=majority', {
+	useNewUrlParser: true,
+	useCreateIndex: true
+}).then(() => {
+	console.log('Connected to DB!');
+}).catch(err => {
+	console.log('ERROR:', err.message);
+});
+
+
 
 app.use(bodyparser.urlencoded({extended: true}));
 app.set("view engine","ejs");	// no need to write .ejs
