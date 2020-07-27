1fd4261cbd51867bd79ff37f08b786e96389de77
Aman <amanakrai98@gmail.com>
2020-07-27 09:19:28 +0000

    add listen

--- app.js
+++ app.js
@@ -75,6 +75,7 @@ app.use(commentRoutes);
 
 
 
-app.listen(5000, function() { 
-  console.log('Server listening on port 5000'); 
-});
+// app.listen(5000, function() { 
+//   console.log('Server listening on port 5000'); 
+// });
+app.listen(process.env.PORT || 5000)
